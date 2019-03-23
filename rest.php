c911953fb4913bd029d10515e7ddd7fc92db9deb
unknown <mjawwadiqbal22@gmail.com>
2019-03-23 15:59:52 -0700

    Http error handled

--- rest.php
+++ rest.php
@@ -1,6 +1,12 @@
 <?php 
+require_once('constants.php');
 class Rest {
     public function __construct(){
+        if($_SERVER['REQUEST_METHOD']!=='POST'){
+            $this->throwError(REQUEST_METHOD_NOT_VALID,
+            'Request method is not valid'
+        );
+        }
 $handler=fopen('php://input','r');
 echo $request= stream_get_contents($handler);
     }
@@ -15,7 +21,9 @@ echo $request= stream_get_contents($handler);
         
     }
     public function throwError($code,$message){
-
+        header("content-type:application/json");
+$errorMsg =json_encode(['Http'=>$code,'status'=>'false','message'=>$message]);
+echo $errorMsg;exit;
     }
     public function  returnResponse(){
 
