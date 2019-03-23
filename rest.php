d6da3658d4c2ff06fd26900d2b1408dd2b0b1566
unknown <mjawwadiqbal22@gmail.com>
2019-03-23 16:09:08 -0700

    --validate method and content type

--- rest.php
+++ rest.php
@@ -1,17 +1,27 @@
 <?php 
 require_once('constants.php');
 class Rest {
+    protected $request;
+    protected $serviceName;
+    protected $param;
+
     public function __construct(){
         if($_SERVER['REQUEST_METHOD']!=='POST'){
             $this->throwError(REQUEST_METHOD_NOT_VALID,
             'Request method is not valid'
         );
         }
-$handler=fopen('php://input','r');
-echo $request= stream_get_contents($handler);
+    $handler=fopen('php://input','r');
+    $this->request= stream_get_contents($handler);
+    $this->validateRequest();
     }
-    public function validateRequest(){
 
+    public function validateRequest(){
+    if($_SERVER['CONTENT_TYPE']!=='application/json'){
+        $this->throwError(REQUEST_CONTENTTYPE_NOT_VALID,'Request Content type is not valid');
+    }
+    $data=json_decode($this->request,true);
+    print_r($data);
     } 
     public function processApi(){
 
