a9b29d37e974f28534e4bcab3c8cf162984d7afa
unknown <mjawwadiqbal22@gmail.com>
2019-03-23 15:04:57 -0700

    --rest and index

---
+++ rest.php
@@ -0,0 +1,23 @@
+<?php 
+class Rest {
+    public function __construct(){
+
+    }
+    public function validateRequest(){
+
+    }
+    public function processApi(){
+
+    }
+    public function validateParameter($fieldName,$value,$dataType,$required)
+    {
+        
+    }
+    public function throwError($code,$message){
+
+    }
+    public function  returnResponse(){
+
+    }
+}
+?>
