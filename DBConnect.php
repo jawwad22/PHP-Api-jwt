41447ff4802c68dee5b664cd1d28e3ed55d91dc7
unknown <mjawwadiqbal22@gmail.com>
2019-03-23 19:59:30 -0700

    --DBConnect

---
+++ DBConnect.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class DbConnect
+{
+   private $server='localhost';
+    private $dbname='jwtapi';
+    private $user ='root';
+    private $pass='';
+   public function connect(){
+    try {
+        $conn=new PDO('mysql:host='.$this->server.';dbname='.$this->
+        dbname,$this->user,$this->pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+      return $conn
+        $db=new DBConnect;
+        $db->connect();
+        echo 'DataBase Connected'
+    } catch (Exception $e) {
+        echo "Database Error:".$e->getMessage();
+    }
+
+}
+}
+
+?>
